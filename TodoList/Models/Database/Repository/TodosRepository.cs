985f56cb1485dee9b98b94b394b33bb7152e22a4
Vitaliy Lubimov <ushock.it@gmail.com>
2021-04-20 18:36:22 +0300

    [TODO-1] Add DB

---
+++ TodoList/Models/Database/Repository/TodosRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TodoList.Models.Database.Entity;
+
+namespace TodoList.Models.Database.Repository
+{
+    public class TodosRepository : IRepository<ToDoItem>
+    {
+        public void Create(ToDoItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
