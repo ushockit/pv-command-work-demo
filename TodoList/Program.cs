e2391617348315eadc37631806adc7e6de5de2e0
Vitaliy Lubimov <ushock.it@gmail.com>
2021-04-20 18:23:12 +0300

    Initial

---
+++ TodoList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoList
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
